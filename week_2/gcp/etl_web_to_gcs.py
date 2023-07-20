2099a79dc22acfef7cc88408b35bc8fb77f9a500
amiraflak <mehrdad.aflakparast@gmail.com>
2023-07-20 22:47:25 +0000

    add: load_to_gcs task

--- week_2/gcp/etl_web_to_gcs.py
+++ week_2/gcp/etl_web_to_gcs.py
@@ -4,7 +4,6 @@ from pathlib import Path
 from prefect import task, flow
 from prefect_gcp.cloud_storage import GcsBucket
 
-
 @task(retries=3)
 def fetch(url: str) -> pd.DataFrame:
     """Read taxi data from web into pandas DataFrame"""
@@ -32,6 +31,14 @@ def to_parquet_and_save(df: pd.DataFrame, dataset_file: str) -> Path:
     
     return path
 
+@task(log_prints=True)
+def load_to_gcs(path: Path) -> None:
+    """Upload dataset into gcs"""
+    
+    gcs_bucket = GcsBucket.load("rides-gcs")
+    gcs_bucket.upload_from_path(from_path=path,
+                                to_path=path)
+    return
 
 @flow(log_prints=False)
 def etl_web_to_gcs() -> None:
@@ -42,10 +49,12 @@ def etl_web_to_gcs() -> None:
     month = 2
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
-
+        
     df = fetch(dataset_url)
     tr_df = transform(df)
     df_parquet_path = to_parquet_and_save(tr_df, dataset_file)
+    load_to_gcs(df_parquet_path)
+    
 
 if __name__ == '__main__':
     etl_web_to_gcs()
