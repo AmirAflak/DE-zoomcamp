d20261d0c20b49244d71aea57b01ab31c92a16dc
amiraflak <mehrdad.aflakparast@gmail.com>
2023-07-21 19:02:28 +0000

    add: to_bq task to write dataframe into bigQuery

--- week_2/gcp/etl_gcs_to_bq.py
+++ week_2/gcp/etl_gcs_to_bq.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 import pandas as pd
 from pathlib import Path
+from dotenv import load_dotenv
 from prefect import task, flow
 from prefect_gcp.cloud_storage import GcsBucket
 from prefect_gcp import GcpCredentials
+from prefect_gcp.bigquery import BigQueryWarehouse
+import os
+
+
 
 @task(retries=3)
 def extract_from_gcs(color: str, year: int, month: int) -> Path:
@@ -18,6 +23,22 @@ def extract_from_gcs(color: str, year: int, month: int) -> Path:
 def transform(path: Path) -> pd.DataFrame:
     """Convert to dataFrame and data cleaning"""
     return pd.read_parquet(path)
+
+@task()
+def to_bq(df: pd.DataFrame) -> None:
+    """Write dataFrame to big query"""
+    load_dotenv()
+    gcp_credentials_block = GcpCredentials.load("rides-gcs-creds")
+    
+    dataframe.to_gbq(
+                destination_table=os.getenv('BQ_TABLE_NAME'), 
+                project_id=os.getenv('BQ_PROJECT_ID'),
+                credentials=gcp_credentials_block.get_credentials_from_service_account(),
+                chunksize=500_000, 
+                if_exists='append') 
+    return
+    
+
     
 
 @flow(log_prints=True)
@@ -25,10 +46,13 @@ def etl_gcs_to_bq():
     """ETL flow to get data from gcs and insert that into BigQuery""" 
     color = "yellow"
     year = 2020
-    month = 2
+    month = 2 
     
     path = extract_from_gcs(color, year,  month)
+    # transformed data
     tr_data = transform(path)
+    # write to big query
+    to_bq(tr_data)
     
 if __name__ == '__main__':
     etl_gcs_to_bq()
