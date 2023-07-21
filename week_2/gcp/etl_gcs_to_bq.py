46bfa14e347298958912024f995c7207fe53a735
amiraflak <mehrdad.aflakparast@gmail.com>
2023-07-21 14:57:46 +0000

    fix: download_object_to_path

--- week_2/gcp/etl_gcs_to_bq.py
+++ week_2/gcp/etl_gcs_to_bq.py
@@ -10,7 +10,7 @@ def extract_from_gcs(color: str, year: int, month: int) -> Path:
     """Download file from gcs and save it locally"""
     gcs_path = f"{color}_tripdata_{year}-{month:02}.parquet"
     gcs_bucket = GcsBucket.load("rides-gcs")
-    gcs_bucket.get_directory(from_path=gcs_path)
+    gcs_bucket.download_object_to_path(from_path=gcs_path)
     
     return Path(f"{gcs_path}")
 
@@ -18,8 +18,8 @@ def extract_from_gcs(color: str, year: int, month: int) -> Path:
 def etl_gcs_to_bq():
     """ETL flow to get data from gcs and insert that into BigQuery""" 
     color = "yellow"
-    year = 2021
-    month = 1
+    year = 2020
+    month = 2
     
     path = extract_from_gcs(color, year,  month)
     
